f98b271fcd0574f2c3b15d9a3bf96e41d42222ef
Pret D.B <pret-db@pret-page.com>
2017-07-20 15:44:59 +0800

    Mainly did some refactoration job, next step is check message system and networking, then ask Shaohua to design a weight/speed model. and teach Lin to design some game map.

---
+++ Assets/script/control/controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Security.Cryptography;
+using UnityEditor;
+
+namespace AssemblyCSharp
+{
+	public abstract class controller : MonoBehaviour
+	{
+		public bool isComputer;
+
+		protected physic physicModel;
+
+		protected abstract void Init();
+
+		protected abstract void UpdateCurrentSpeedVector();
+	}
+}
+
