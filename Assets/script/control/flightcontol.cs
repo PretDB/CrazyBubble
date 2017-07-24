7f6952f73134e3a59e5f9772bbbbb2765e26822a
Pret D.B <pret-db@pret-page.com>
2017-07-24 17:29:25 +0800

    Network experiment finished, see sence 'network'

---
+++ Assets/script/control/flightcontol.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.CompilerServices;
+using UnityEngine.Windows.Speech;
+using UnityEngine.Events;
+
+public class flightcontol : NetworkBehaviour
+{
+
+    public float rotateSpeed = 3f;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GameObject.Find("Camera").GetComponent<netCameraFollow>().target = this.gameObject;
+        }
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        this.LocalMove(); 
+    }
+
+    void LocalMove()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 v = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                v = new Vector3(0, v.y + this.rotateSpeed * Time.deltaTime * 5, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                v = new Vector3(0, v.y - this.rotateSpeed * Time.deltaTime * 5, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                v = new Vector3(0, v.y, -this.rotateSpeed * Time.deltaTime * 20);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                v = new Vector3(0, v.y, +this.rotateSpeed * Time.deltaTime * 20);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                v = new Vector3(+this.rotateSpeed * Time.deltaTime * 20, v.y, v.z);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                v = new Vector3(-this.rotateSpeed * Time.deltaTime * 20, v.y, v.z);
+            }
+            this.CmdRemoteMove(v);
+        }  
+    }
+
+    [Command]
+    void CmdRemoteMove(Vector3 v)
+    {
+        this.transform.Translate(new Vector3(0, 0, v.y));
+        this.transform.Rotate(new Vector3(0, v.z, v.x));
+    }
+}
---
+++ Assets/script/view/netCameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class netCameraFollow : NetworkBehaviour
+{
+
+    public GameObject target;
+    public Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 v = this.target.transform.position + this.offset;
+            //this.transform.position = this.target.transform.position + this.offset;
+            this.transform.position = Vector3.Lerp(this.transform.position, v, Time.deltaTime * 2);
+        }
+    }
+}
